e44f0fd7ff95d0169c26711244f2a8c956644197
Brandon <bredmann245@gmail.com>
2020-10-08 22:34:33 -0500

    cleaned up unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
-import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
-import { CardsComponent } from './shared/cards/cards.component';
 import { ConfigService } from './config/config.service';
-import { SubnavComponent } from './shared/subnav/subnav/subnav.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    FooterComponent,
-    HeaderComponent,
-    CardsComponent,
-    SubnavComponent,
-  ],
+  declarations: [AppComponent, NavbarComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [ConfigService],
   bootstrap: [AppComponent],
